604a1d03c3d168d9b9ef97807ddc09f6311e9d24
Yox2Yox <ingentity@gmail.com>
2020-05-31 13:33:23 +0900

    不要なファイルを削除

--- components/pages/HomePage.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-class HomePage extends React.Component{
-    render(){
-        <View style={styles.container}>
-            <Text>"あああ"</Text>
-        </View>
-    }
-}
